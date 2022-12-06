ef8315b2b69e8680cd5e1c4001ce81e5e6c234cf
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-12-06 19:02:45 +0530

    base: Add optional haptic feedback on scrolling with fling gesture [1/2]
    - The device vibrates with a click effect on reaching the edge of the view after a successfull fling scroll
    - Add a permission check to prevent apps without vibrate permission from crashing

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5671,6 +5671,13 @@ public final class Settings {
         public static final String VOLUME_PANEL_HAPTIC_FEEDBACK = "volume_panel_haptic_feedback";
 
         /**
+         * Haptic feedback on scroll fling
+         * @hide
+         */ 
+        @Readable
+        public static final String SCROLL_FLING_HAPTIC_FEEDBACK = "scroll_fling_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
